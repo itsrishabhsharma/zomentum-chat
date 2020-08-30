1382fe631ace3af8652fa424540b035e45aacd5d
Rishabh Sharma <itsrishabhsharma@gmail.com>
2020-08-30 17:53:04 +0530

    first commit designed chat-sidebar

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUN394xxV27xlkLWJsmCYUjsTNhycuCBY",
+    authDomain: "zomentum-chat.firebaseapp.com",
+    databaseURL: "https://zomentum-chat.firebaseio.com",
+    projectId: "zomentum-chat",
+    storageBucket: "zomentum-chat.appspot.com",
+    messagingSenderId: "829660876038",
+    appId: "1:829660876038:web:5554b85439d44982de6287"
+  };
