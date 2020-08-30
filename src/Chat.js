b7925a260496e2e400cce8d29673f7d8cf24d2e0
Rishabh Sharma <itsrishabhsharma@gmail.com>
2020-08-30 21:18:37 +0530

    built chatbox to send/reply messages

--- src/Chat.js
+++ src/Chat.js
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from 'react'
 import { Avatar, IconButton } from "@material-ui/core";
 import "./Chat.css"
 import { AttachFile, SearchOutlined, MoreVert } from '@material-ui/icons';
-
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
+import MicIcon from "@material-ui/icons/Mic"
 function Chat() {
+
+
+    const [input, setInput] = useState("");
 
     const [seed, setSeed] = useState("");
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
     }, []);
+
+    const sendMessage = (e) => {
+e.preventDefault();
+
+console.log("You typed >>> ",input);
+
+setInput("");
+    };
 
     return (
         <div className="chat">
@@ -43,17 +55,30 @@ function Chat() {
                         </div>
                 <div className="chat__body">
 
-<p className="chat__message">Hey man!</p>
+<p className={`chat__message ${true &&  
+"chat__reciever"}`}>
+    <span className="chat__name">Rishabh Sharma</span>
+    Hey welcome to Zomentum Chat!
 
+    <span className="chat__timestamp">
+        2:20pm
+    </span>
+</p>
                 </div>
 
 
                 <div className="chat__footer">
+                    <InsertEmoticonIcon />
+                    <form>
+                        <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text" />
+                        <button onClick={sendMessage} type="submit"> Send a message </button>
+                        </form>
 
-                </div>
-
-            </div>
-    )
+                        <MicIcon />
+                        </div>
+                        </div>
+                        
+    );
 }
 
 export default Chat
--- src/firebase.js
+++ src/firebase.js
@@ -7,3 +7,15 @@ const firebaseConfig = {
     messagingSenderId: "829660876038",
     appId: "1:829660876038:web:5554b85439d44982de6287"
   };
+
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+  const auth = firebase.auth.GoogleAuthProvider();
+
+
+  export { auth, provider };
+
+  export default db;
